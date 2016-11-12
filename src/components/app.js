731602c2c6a0cb55f29910b226616161e51f7882
Dion <Dion.Fulwood@gmail.com>
2016-11-11 20:34:06 -0800

    Adds components and container to app

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import SearchBar from "../containers/search-bar";
+import PostList from "../containers/post_list";
+import FavoriteList from "../containers/favorite_list";
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>Working</div>
+      <div className="inner-container">
+        <SearchBar />
+        <FavoriteList />
+        <PostList />
+      </div>
     );
   }
 }
