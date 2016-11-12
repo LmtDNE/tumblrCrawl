8580debb0e75f0c5a981a7f8b00e6b8244d22beb
Dion <Dion.Fulwood@gmail.com>
2016-11-11 22:17:44 -0800

    Adds headers

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
