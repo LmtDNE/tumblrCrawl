868bfa98485ce49c622f4d6d3e8ce1b48a40b480
Dion <Dion.Fulwood@gmail.com>
2016-11-11 23:44:18 -0800

    Refactors actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 import fetchJsonp from 'fetch-jsonp';
-const API_KEY  = Private.C_API_KEY;
+import Private from '../../private';
+const API_KEY  =  Private.C_API_KEY;
 
 
 export const ROOT_URL = "https://api.tumblr.com/v2/";
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const ADD_FAVORITE = 'ADD_FAVORITE';
-export const REMOVE_FAVORITE = 'REMOVE_FAVORITE';
+export const FETCH_POSTS = "FETCH_POSTS";
+export const ADD_FAVORITE = "ADD_FAVORITE";
+export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
 
 
 export function addToFavorites(post) {
-  console.log("This is post inside addToFavorite", post);
   return { 
     type: ADD_FAVORITE,
     payload: post
@@ -18,7 +18,6 @@ export function addToFavorites(post) {
 }
 
 export function removeFromFavorites(id) {
-  console.log("removeFromFavorites is being called", id);
   return {
     type: REMOVE_FAVORITE,
     payload: id
@@ -32,12 +31,10 @@ export function fetchPosts(blog, tag) {
   if(blog && !tag){
     url = `${ROOT_URL}blog/${blog}/posts?api_key=${API_KEY}&limit=${resultLimit}`;
     return dispatch => {
-      return fetchJsonp(url).then(function(response) {
-        console.log("This is response", response);
+      return fetchJsonp(url).then(response => {
         return response.json()
       }).then(
         request => {
-          console.log("This is request", request);
           const blogPosts = request.response.posts
           dispatch({
           type: FETCH_POSTS,
@@ -53,7 +50,7 @@ export function fetchPosts(blog, tag) {
     url = `${ROOT_URL}tagged?api_key=${API_KEY}&tag=${tag}&limit=${resultLimit}`;
     console.log("This is url", url);
     return dispatch => {
-      return fetchJsonp(url).then(function(response) {
+      return fetchJsonp(url).then(response => {
         return response.json()
       }).then(
         request => {
@@ -71,7 +68,7 @@ export function fetchPosts(blog, tag) {
   if(blog && tag){
     url = `${ROOT_URL}blog/${blog}/posts?api_key=${API_KEY}&tag=${tag}&limit=${resultLimit}`;
     return dispatch => {
-      return fetchJsonp(url).then(function(response) {
+      return fetchJsonp(url).then(response => {
         return response.json()
       }).then(
         request => {
